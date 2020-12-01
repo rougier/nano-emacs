a1174b63ce1f3b6a79c2adab2a466d3137766df0
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-12-01 07:31:39 +0100

    Replace ⌘ with M

--- nano-help.el
+++ nano-help.el
@@ -17,8 +17,8 @@
 ;; ---------------------------------------------------------------------
 ;; 
 ;; This file defines the help message and bind it to M-h
-;; [C-x C-f] Open  [⌘-w] Copy   [C-w] Cut   [C-s] Search  [C-g]   Cancel
-;; [C-x C-s] Save  [C-y] Paste  [C-/] Undo  [⌘-x] Command [C-x C-c] Quit
+;; [C-x C-f] Open  [M-w] Copy   [C-w] Cut   [C-s] Search  [C-g]   Cancel
+;; [C-x C-s] Save  [C-y] Paste  [C-/] Undo  [M-x] Command [C-x C-c] Quit
 ;;
 ;; Usage:
 ;;
@@ -32,10 +32,10 @@
   (let ((message-log-max nil))
     (message
      (concat
-      " [C-x C-f] Open  [⌘-w] Copy   [C-w] Cut   [C-s] Search           "
+      " [C-x C-f] Open  [M-w] Copy   [C-w] Cut   [C-s] Search           "
       (propertize "[C-g]   Cancel" 'face 'bold)
       "\n"
-      " [C-x C-s] Save  [C-y] Paste  [C-/] Undo  [⌘-x] Command          "
+      " [C-x C-s] Save  [C-y] Paste  [C-/] Undo  [M-x] Command          "
       (propertize "[C-x C-c] Quit" 'face 'bold)))
     (sit-for 30)))
 
