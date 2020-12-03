5954fe22d9d7e6ccab5832b9b4899f5e15462c82
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-12-03 20:08:04 +0100

    Darken hl line color

--- nano-theme-light.el
+++ nano-theme-light.el
@@ -20,7 +20,7 @@
 ;; Colors from Material design at https://material.io/
 (defvar nano-color-foreground "#37474F") ;; Blue Grey / L800
 (defvar nano-color-background "#FFFFFF") ;; White
-(defvar nano-color-highlight  "#FAFAFA") ;; Very Light Grey
+(defvar nano-color-highlight  "#F9F9F9") ;; Very Light Grey
 (defvar nano-color-critical   "#FF6F00") ;; Amber / L900
 (defvar nano-color-salient    "#673AB7") ;; Deep Purple / L500
 (defvar nano-color-strong     "#000000") ;; Black
