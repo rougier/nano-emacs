86a7ee9de4bf3b329200c6ad5b323326d130189f
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-12-03 19:41:57 +0100

    Fix a OSX term/zsh display bug with displya-table

--- nano-layout.el
+++ nano-layout.el
@@ -18,8 +18,8 @@
 
 (setq default-frame-alist
       (append (list
-	       '(font . "Roboto Mono:style=Light:size=14")
-;;	       '(font . "Roboto Mono Emacs Regular 14")
+;;	       '(font . "Roboto Mono:style=Light:size=14")
+	       '(font . "Roboto Mono Emacs Regular:size=14")
 	       '(min-height . 1)  '(height     . 45)
 	       '(min-width  . 40) '(width      . 81)
                '(vertical-scroll-bars . nil)
@@ -45,7 +45,9 @@
                         (make-glyph-code ?… 'fallback))
 (set-display-table-slot standard-display-table 'wrap
                          (make-glyph-code ?↩ 'fallback))
-
+;; Fix bug on OSX in term mode & zsh (spurious % after each command)
+(add-hook 'term-mode-hook
+	  (lambda () (setq buffer-display-table (make-display-table))))
 
 (setq inhibit-startup-screen t
       inhibit-startup-message t
