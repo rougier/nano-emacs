0c128277053f249ac519633d578bc1eafe000f0a
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-12-05 09:26:30 +0100

    Some sane defaults

---
+++ nano-defaults.el
@@ -0,0 +1,120 @@
+;; ---------------------------------------------------------------------
+;; GNU Emacs / N Λ N O - Emacs made simple
+;; Copyright (C) 2020 - N Λ N O developers
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;; ---------------------------------------------------------------------
+
+;; No startup  screen
+(setq inhibit-startup-screen t)
+
+;; No startup message
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+
+;; No message in scratch buffer
+(setq initial-scratch-message nil)
+
+;; Initial buffer 
+(setq initial-buffer-choice nil)
+
+;; No frame title
+(setq frame-title-format nil)
+
+;; No file fialog
+(setq use-file-dialog nil)
+
+;; No dialog box
+(setq use-dialog-box nil)
+
+;; No popup windows
+(setq pop-up-windows nil)
+
+;; User name
+(setq user-full-name "Nicolas P. Rougier")
+
+;; User mail address
+(setq user-mail-address "Nicolas.Rougier@inria.fr")
+      
+;; No empty line indicators
+(setq indicate-empty-lines nil)
+
+;; No cursor in inactive windows
+(setq cursor-in-non-selected-windows nil)
+
+;; Text mode is initial mode
+(setq initial-major-mode 'text-mode)
+
+;; Text mode is default major mode
+(setq default-major-mode 'text-mode)
+
+;; Moderate font lock
+(setq font-lock-maximum-decoration nil)
+
+;; No limite on font lock
+(setq font-lock-maximum-size nil)
+
+;; No line breat space points
+(setq auto-fill-mode nil)
+
+;; Fill column at 80
+(setq fill-column 80)
+
+;; No confirmation for visiting non-existent files
+(setq confirm-nonexistent-file-or-buffer nil)
+
+;; Completion style, see
+;; gnu.org/software/emacs/manual/html_node/emacs/Completion-Styles.html
+(setq completion-styles '(substring substring))
+
+;; No scroll bars
+(scroll-bar-mode 0)
+
+;; No toolbar
+(tool-bar-mode 0)
+
+;; No menu bar
+(if (display-graphic-p)
+    (menu-bar-mode t) ;; When nil, focus problem on OSX
+  (menu-bar-mode -1))
+
+;; Pixel scroll (as opposed to char scrool)
+(pixel-scroll-mode t)
+
+;; Mac specific
+(setq ns-use-native-fullscreen t
+      mac-option-key-is-meta nil
+      mac-command-key-is-meta t
+      mac-command-modifier 'meta
+      mac-option-modifier nil
+      mac-use-title-bar nil)
+
+;; y/n for  answering yes/no questions
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; No tabs
+(setq-default indent-tabs-mode nil)
+
+;; Tab.space equivalence
+(setq tab-width 4)
+
+;; Buffer encoding
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-language-environment   'utf-8)
+(set-language-environment   'utf-8)
+
+(provide 'nano-defaults)
