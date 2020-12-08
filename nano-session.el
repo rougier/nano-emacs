80db68473d3a0700a19875c33ce3407a7731c566
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-12-08 13:17:08 +0100

    Added backup directory settings

--- nano-session.el
+++ nano-session.el
@@ -64,4 +64,20 @@
 (setq bookmark-default-file "~/.nano-bookmark")
 
 
+;; Backup
+(setq backup-directory-alist '(("." . "~/.backups"))
+      make-backup-files t     ; backup of a file the first time it is saved.
+      backup-by-copying t     ; don't clobber symlinks
+      version-control t       ; version numbers for backup files
+      delete-old-versions t   ; delete excess backup files silently
+      kept-old-versions 6     ; oldest versions to keep when a new numbered
+                              ;  backup is made (default: 2)
+      kept-new-versions 9     ; newest versions to keep when a new numbered
+                              ;  backup is made (default: 2)
+      auto-save-default t     ; auto-save every buffer that visits a file
+      auto-save-timeout 20    ; number of seconds idle time before auto-save
+                              ;  (default: 30)
+      auto-save-interval 200)  ; number of keystrokes between auto-saves
+                              ;  (default: 300)
+
 (provide 'nano-session)
