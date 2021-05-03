4c0fcae94bab9237a4e27231f3cb00f25384b301
Norman Walsh <ndw@nwalsh.com>
2021-05-03 21:42:46 +0100

    Revert unrelated change to default-frame-alist

--- nano-layout.el
+++ nano-layout.el
@@ -20,7 +20,7 @@
 
 (setq default-frame-alist
       (append (list
-	       '(font . "Roboto Mono:style=Medium:size=18")
+	       '(font . "Roboto Mono:style=Light:size=14")
 	       ;; '(font . "Roboto Mono Emacs Regular:size=14")
 	       '(min-height . 1)  '(height     . 45)
 	       '(min-width  . 1) '(width      . 81)
