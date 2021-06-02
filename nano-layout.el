6667e4bf5b1e49f3172bcd12745fc348fb8e1910
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2021-06-02 21:03:51 +0200

    1 pixel fringe to cancel it

--- nano-layout.el
+++ nano-layout.el
@@ -20,15 +20,14 @@
 
 (setq default-frame-alist
       (append (list
-               ;; Font size and family is set by nano-face-default
-	       ;;'(font . "Roboto Mono:style=Light:size=14")
-	       ;; '(font . "Roboto Mono Emacs Regular:size=14")
-	       '(min-height . 1)  '(height     . 45)
-	       '(min-width  . 1) '(width      . 81)
+	           '(min-height . 1)
+               '(height     . 45)
+	           '(min-width  . 1)
+               '(width      . 81)
                '(vertical-scroll-bars . nil)
                '(internal-border-width . 24)
-               '(left-fringe    . 0)
-               '(right-fringe   . 0)
+               '(left-fringe    . 1)
+               '(right-fringe   . 1)
                '(tool-bar-lines . 0)
                '(menu-bar-lines . 0))))
 
@@ -61,6 +60,7 @@
       initial-scratch-message nil)
 (if (fboundp 'tool-bar-mode) (tool-bar-mode nil))
 (tooltip-mode 0)
+(scroll-bar-mode nil)
 (menu-bar-mode 0)
 ;; (global-hl-line-mode 1)
 (setq x-underline-at-descent-line t)
