9fc1e1ae9145d676b3bffd9a672ebac8881959e8
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2021-10-07 12:57:18 +0200

    Fix #100

--- nano-defaults.el
+++ nano-defaults.el
@@ -41,12 +41,6 @@
 ;; No popup windows
 (setq pop-up-windows nil)
 
-;; User name
-(setq user-full-name "Nicolas P. Rougier")
-
-;; User mail address
-(setq user-mail-address "Nicolas.Rougier@inria.fr")
-
 ;; No empty line indicators
 (setq indicate-empty-lines nil)
 
