1076d8c32824ddd6b9eb2ae34f5ba08893735031
jonroby <35011179+jonroby@users.noreply.github.com>
2025-05-07 11:59:42 -0400

    poimandres colors

--- nano-theme-dark.el
+++ nano-theme-dark.el
@@ -18,20 +18,20 @@
 (require 'nano-base-colors)
 
 (defun nano-theme-set-dark ()
+  "Apply dark Nano theme base (Poimandres port)."
   "Apply dark Nano theme base."
   ;; Colors from Nord theme at https://www.nordtheme.com
   (setq frame-background-mode     'dark)
   (setq nano-color-foreground "#ECEFF4") ;; Snow Storm 3  / nord  6
-  (setq nano-color-background "#2E3440") ;; Polar Night 0 / nord  0
+  (setq nano-color-background "#1C1E28") ;; Polar Night 0 / nord  0 [JR-CHANGE] Background
   (setq nano-color-highlight  "#3B4252") ;; Polar Night 1 / nord  1
   (setq nano-color-critical   "#EBCB8B") ;; Aurora        / nord 11
   (setq nano-color-salient    "#81A1C1") ;; Frost         / nord  9
-  (setq nano-color-strong     "#ECEFF4") ;; Snow Storm 3  / nord  6
-  (setq nano-color-popout     "#D08770") ;; Aurora        / nord 12
-  (setq nano-color-subtle     "#434C5E") ;; Polar Night 2 / nord  2
-  (setq nano-color-faded      "#677691") ;;
+  (setq nano-color-strong     "#ECEFF4") ;; Snow Storm 3  / nord  6 [NO-CHANGE] Off White
+  (setq nano-color-popout     "#58D4BB") ;; Aurora        / nord 12 [JR-CHANGE] Lime green
+  (setq nano-color-subtle     "#252934") ;; Polar Night 2 / nord  2 [JR-CHANGE] Modeline
+  (setq nano-color-faded      "#373B4C") ;;                         [JR-CHANGE] Comments
   ;; to allow for toggling of the themes.
-  (setq nano-theme-var "dark")
-  )
+  (setq nano-theme-var "dark")) 
 
 (provide 'nano-theme-dark)
